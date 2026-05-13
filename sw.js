a96a11f35c05d6956b4df070e084046fa99ff0e8
Ostapker <okernytskiy@gmail.com>
2026-05-13 20:46:54 +0000

    Init commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'pdr-v1';
+const STATIC_ASSETS = [
+  './index.html',
+  './questions_data.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        // Cache images on-demand
+        if (e.request.url.includes('/images/')) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
